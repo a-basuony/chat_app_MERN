5952bd5de74a751946564c7f016b9a1e7778ce82
Ahmed Basuony <A.Basuony.social@gmail.com>
2025-11-04 20:24:40 +0200

    applying login logic

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -7,6 +7,7 @@ const useAuthStore = create((set) => ({
   authUser: null,
   isCheckingAuth: false,
   isSigningUP: false,
+  isLoggingIn: false,
 
   // ✅ Check Auth (for persisted session)
   checkAuth: async () => {
@@ -30,7 +31,7 @@ const useAuthStore = create((set) => ({
     } catch (error) {
       console.log("Error in signup:", error);
       set({ isSigningUP: false });
-const errRes = error.response?.data;
+      const errRes = error.response?.data;
 
     // If there are validation errors, show the first one
         if (errRes?.errors?.length) {
@@ -41,6 +42,29 @@ const errRes = error.response?.data;
           toast.error("Something went wrong!");
         }    
       }
+  },
+
+  // ✅ Login
+  login: async (data) => {
+    set({ isLoggingIn: true });
+    try {
+      const res = await api.post("/auth/login", data); // your backend route
+      set({ authUser: res.data, isLoggingIn: false });
+      toast.success("Login successful!");
+    } catch (error) {
+      console.log("Error in login:", error);
+      set({ isLoggingIn: false });
+            const errRes = error.response?.data;
+
+    // If there are validation errors, show the first one 
+        if (errRes?.errors?.length) {
+          toast.error(errRes.errors[0].msg);
+        } else if (errRes?.message) {
+          toast.error(errRes.message);
+        } else {
+          toast.error("Login failed!");
+    }
+    }
   },
 }));
 
