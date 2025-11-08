2edecfe3738f1aecc2c4a2c76d9bedb47e99ab13
Ahmed Basuony <A.Basuony.social@gmail.com>
2025-11-08 15:42:50 +0200

    creating chatlist logic

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react'
 import useAuthStore from './../store/useAuthStore';
 import ChatHeader from './ChatHeader'
 import { useChatStore } from './../store/useChatStore';
+import NoChatHistoryPlaceholder from './NoChatHistoryPlaceholder';
 
 const ChatContainer = () => {
   const {selectedUser , getMessagesByUserId, messages} = useChatStore()
@@ -17,6 +18,26 @@ const ChatContainer = () => {
   return (
     <>
     <ChatHeader/>
+    <div className="flex-1 px-6 overflow-y-auto py-8">
+      {messages.length > 0 ? (
+        <div className="max-w-3xl mx-auto space-y-6">
+          {messages.map((message) => (
+            <div key={message._id}
+              className={`chat ${message.senderId === authUser._id ? "chat-end" : "chat-start"}`}
+              >
+                <div className={`chat-bubble  relative ${message.senderId === authUser._id ? "bg-cyan-600 text-white" : "bg-slate-800 text-slate-200"}`}>
+                  {message.image && (
+                    <img src={message.image} alt={message.text} className='rounded-lg h-48 object-cover'/>
+                  )}
+                  {message.text && <p className='mt-2'>{message.text}</p>}
+                </div>
+              </div>
+          ))}
+        </div>
+      ): (
+        <NoChatHistoryPlaceholder name={selectedUser.name} />
+      )}
+    </div>
     {/* <ChatMessages/>
     <ChatInput/> */}
     </>
